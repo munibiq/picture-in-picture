32d9f81e4eb6a193072de1b943ab620bbb88aafe
munibiq <munibiqbal@arcor.de>
2023-11-23 23:15:09 +0100

    initial upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const videoElement = document.getElementById('video')
+
+const button = document.getElementById('button')
+
+// Prompt to select media stream, pass to video 
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error) {
+        // Catch error
+        console.log('something went wrong', error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
